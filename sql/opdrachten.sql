dd852c1553e7131a13592c827e074c6367672739
LiekeDeckers <liekedeckers15@gmail.com>
2023-04-20 11:46:57 +0200

    opdrachten #3

--- sql/opdrachten.sql
+++ sql/opdrachten.sql
@@ -213,17 +213,150 @@ SELECT  CompanyName,
 FROM    customers
 WHERE   country = 'Mexico' OR country = 'Spain' AND city != 'Madrid';
 
---30. Firstname, Lastname can be reached at x Extension as ContactInfo
-SELECT  FirstName,
-        LastName,
-        Extension
+--30. Firstname, Lastname can be reached at xExtension as ContactInfo
+SELECT  CONCAT(FirstName, ' ', LastName, ' ', 'can be reached at', ' ', 'x', Extension) AS ContactInfo
 FROM    employees;
 
+--31. ContactName of all customers that do not have letter A as the second alphabet in their Contactname
+SELECT  ContactName
+FROM    customers
+WHERE   ContactName NOT LIKE '_a%';
 
+--32.  average UnitPrice rounded to the next whole number, total price of UnitsInStock and maximum number of orders 
+-- from the products table. All saved as AveragePrice, TotalStock and MaxOrder respectively.
+SELECT  ROUND(AVG(UnitPrice)) AS AveragePrice,
+        SUM(UnitPrice * UnitsInStock) AS TotalPrice,
+        MAX(UnitsOnOrder) AS MaxOrder
+FROM    products;
 
+--33. SupplierID, CompanyName, CategoryName, ProductName and UnitPrice 
+--from the products, suppliers and categories table
+SELECT  products.SupplierID,
+        suppliers.CompanyName,
+        categories.CategoryName,
+        products.ProductName,
+        products.UnitPrice
+FROM    products
+JOIN    suppliers ON products.SupplierID = suppliers.SupplierID 
+JOIN    categories ON products.CategoryID = categories.CategoryID;
 
+--34. CustomerID, sum of Freight, from the orders table with sum of freight greater $200, grouped by CustomerID
+SELECT  CustomerID,
+        SUM(Freight) AS sumOfFreight
+FROM    orders
+GROUP BY CustomerID
+HAVING  SUM(Freight) > 200;
 
+--35. OrderID ContactName, UnitPrice, Quantity, Discount 
+-- from the order details, orders and customers table with discount given on every purchase.
+SELECT  orders.OrderID,
+        customers.ContactName,
+        order_details.UnitPrice,
+        order_details.Quantity,
+        order_details.Discount 
+FROM    order_details
+JOIN    orders ON orders.OrderID = order_details.OrderID
+JOIN    customers ON customers.CustomerID = orders.CustomerID;
 
+--36. EmployeeID, the LastName and FirstName as employee, and the LastName and FirstName 
+-- of who they report to as manager from the employees table sorted by Employee ID.
+SELECT  A.EmployeeID,
+        CONCAT(A.FirstName, ' ', A.LastName) AS employee,
+        CONCAT(B.FirstName, ' ', B.LastName) AS manager
+FROM    employees A
+JOIn    employees B ON A.ReportsTo = B.EmployeeID 
+ORDER BY EmployeeID;
 
+--37. the average, minimum and maximum UnitPrice of all products as AveragePrice, MinimumPrice and MaximumPrice
+SELECT  AVG(UnitPrice) AS AveragePrice,
+        MIN(UnitPrice) AS MinimumPrice,
+        MAX(UnitPrice) AS MaximumPrice
+FROM    products;
 
+--38. a view that shows the CustomerID, CompanyName, ContactName, ContactTitle, 
+-- Address, City, Country, Phone, OrderDate, RequiredDate, ShippedDate from the customers and orders table.
+CREATE VIEW customer_orders AS
+SELECT  customers.CustomerID,
+        customers.CompanyName,
+        customers.ContactName,
+        customers.ContactTitle, 
+        customers.Address, 
+        customers.City,
+        customers.Country,
+        customers.Phone, 
+        orders.OrderDate, 
+        orders.RequiredDate,
+        orders.ShippedDate
+FROM    orders
+JOIN    customers ON customers.CustomerID = orders.CustomerID;
 
+--39. Change the name of the view you created to customer details
+RENAME TABLE customer_orders
+TO customer_details;
+
+--40. a view named ProductDetails that shows the ProductID, CompanyName, ProductName, CategoryName, 
+-- Description, QuantityPerUnit, UnitPrice, UnitsInStock, UnitsOnOrder, ReorderLevel, Discontinued 
+-- from the supplier, products and categories tables
+CREATE VIEW product_details AS
+SELECT  products.ProductID,
+        suppliers.CompanyName,
+        products.ProductName,
+        categories.CategoryName,
+        categories.Description,
+        products.QuantityPerUnit,
+        products.UnitPrice,
+        products.UnitsInStock,
+        products.UnitsOnOrder,
+        products.ReorderLevel,
+        products.Discontinued
+FROM    products
+JOIN    categories ON categories.CategoryID = products.CategoryID
+JOIN    suppliers ON suppliers.SupplierID = products.SupplierID;
+
+--41. Drop the customer details view
+DROP VIEW customer_details;
+
+--42. fetch the first 5 character of categoryName from the category tables and renamed as ShortInfo
+SELECT  LEFT(CategoryName, 5) AS ShortInfo
+FROM    categories
+
+--43. create a copy of the shipper table as shippers_duplicate. 
+CREATE TABLE shippers_duplicate LIKE shippers;
+--Then insert a copy of shippers data into the new table
+INSERT INTO shippers_duplicate SELECT * FROM shippers;
+
+--44. ShipperID, CompanyName, Phone, Email from the shippers_duplicate table
+SELECT  ShipperID, 
+        CompanyName, 
+        Phone, 
+        LOWER(CONCAT(REPLACE(CompanyName, ' ', ''), '@gmail.com')) AS Email 
+FROM    shippers_duplicate;
+
+--45. CompanyName and ProductName from all product in the Seafood category
+SELECT  suppliers.CompanyName, 
+        products.ProductName
+FROM    products
+JOIN    suppliers ON suppliers.SupplierID = products.SupplierID
+JOIN    categories ON categories.CategoryID = products.CategoryID
+WHERE   categories.CategoryName = 'Seafood';
+
+--46. CategoryID, CompanyName and ProductName from all product in the categoryID 5.
+SELECT  products.CategoryID, 
+        suppliers.CompanyName, 
+        products.ProductName
+FROM    products
+JOIN    categories ON categories.CategoryID = products.CategoryID
+JOIN    suppliers ON suppliers.SupplierID = products.SupplierID
+WHERE   products.CategoryID = 5;
+
+--47. Delete the shippers_duplicate table
+DROP TABLE shippers_duplicate;
+
+--48. LastName, FirstName, Title, Age from the employees table
+DECLARE @Now AS datetime
+SELECT  @Now='2023-04-20'
+        LastName, 
+        FirstName,
+        Title,
+        (CONVERT(int,CONVERT(char(8),@Now,112))-CONVERT(char(8),BirthDate,112))/10000 AS Age
+FROM    employees;
